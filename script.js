3e43bad5d360756e06243db1a632e99ab6b9a944
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-08-14 08:55:08 -1000

    8 / 14 / 2017
    Added a JS & HTML file together

---
+++ script.js
@@ -0,0 +1,7 @@
+function sayHello() {
+  //I create a function called sayHello//
+  var userName = prompt("What is your name?");
+  //I create a variable called userName and the user something//
+  window.alert("Hello " + userName);
+  //I create a popup window tellling the userName hello//
+}
