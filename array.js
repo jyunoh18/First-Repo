7d9df869ebce68972396268ebd8767e9298a11a5
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-08-16 08:44:33 -1000

    Create array.js

---
+++ array.js
@@ -0,0 +1,20 @@
+var favFoods = ["bacon", "Lettuce", "Tomato"];
+
+var myChoice = 1;
+
+window.alert(favFoods[myChoice]);
+
+//Lettuce will appear all the time//
+
+
+
+
+
+
+var favFoods = ["bacon", "Lettuce", "Tomato"];
+
+var students = ["Michael", "Raene", "Chuck", "Ryan"]
+
+window.alert(students[2] + " " + favFoods[0]);
+
+//Chuck bacon will appear//
